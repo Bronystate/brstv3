63eea87c495b5cce448a73e72d44aaa37dab17c3
Shizuka <shizuka.kamishima@gmail.com>
2015-11-23 16:43:29 -0600

    added list sorting

--- assets/js/brst-countdowns.js
+++ assets/js/brst-countdowns.js
@@ -18,8 +18,6 @@ var interval; //to hold the setInterval()
 var nextFridayMovie = moment.tz("America/New_York").hour(19).minute(0).second(0).millisecond(0).day(5);
 var nextSaturdayMovie = moment.tz("America/New_York").hour(14).minute(0).second(0).millisecond(0).day(6);
 
-var now = moment();
-
 function loadTimers() {
   console.log("Adding moments to timers...");
   console.log(brst_timers.length);
@@ -28,6 +26,15 @@ function loadTimers() {
     console.log('Loaded timer: "' + brst_timers[i].title + '"');
     brst_timers[i]["moment"] = moment(brst_timers[i].time, "YYYY-MM-DD HH:mm Z");
   }
+  brst_timers.sort(cmpTimers);
+}
+
+function cmpTimers(a, b) {
+  if (a.moment.isBefore(b.moment))
+    return -1;
+  if (a.moment.isAfter(b.moment))
+    return 1;
+  return 0;
 }
 
 window.onload = loadTimers;
