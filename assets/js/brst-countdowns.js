e45251e7f0879fdf79ddaeaefe653c39d3071938
Shizuka <shizuka.kamishima@gmail.com>
2015-11-23 16:33:18 -0600

    added timer loading

--- assets/js/brst-countdowns.js
+++ assets/js/brst-countdowns.js
@@ -8,27 +8,26 @@ var timertitle = document.getElementById("timertitle");
 var timerclock = document.getElementById("timerclock");
 var sidebar = document.getElementById("sidebar");
 
-// -- From brst-timers.js --
-//var showInSidebar = <number of timers to show in sidebar>
-//var brst_timers[] = array of timer structs:
-//  {"title":"Movie Night", "time":"2015-11-27 18:00 -0600", "url":"/path/to/lotto"}
-
 var _second = 1000;
 var _minute = _second * 60;
 var _hour = _minute * 60;
 var _day = _hour * 24;
 
-var timer; //to hold the setInterval()
+var interval; //to hold the setInterval()
 
 var nextFridayMovie = moment.tz("America/New_York").hour(19).minute(0).second(0).millisecond(0).day(5);
-var nextSaturdayMovie = moment.tz("America/New_York").hour(13).minute(0).second(0).millisecond(0).day(6);
+var nextSaturdayMovie = moment.tz("America/New_York").hour(14).minute(0).second(0).millisecond(0).day(6);
 
-var j = 0;
-var timerlist;
 var now = moment();
 
-for (var i = 0; i++; i < showInSidebar) {
-  // i = total timers loaded
-  // j = index in timer data list
-  
+function loadTimers() {
+  console.log("Adding moments to timers...");
+  console.log(brst_timers.length);
+  // add momentjs objects to timers
+  for (var i = 0; i < brst_timers.length; i++) {
+    console.log('Loaded timer: "' + brst_timers[i].title + '"');
+    brst_timers[i]["moment"] = moment(brst_timers[i].time, "YYYY-MM-DD HH:mm Z");
+  }
 }
+
+window.onload = loadTimers;
