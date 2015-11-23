fdf9b81bb7b29cb5b49b5c815d7a5a1454d071bc
Shizuka <shizuka.kamishima@gmail.com>
2015-11-23 13:57:02 -0600

    added initial prep for timers

---
+++ assets/js/brst-countdowns.js
@@ -0,0 +1,10 @@
+/**
+ * Bronystate 3.0 - Countdown Timers
+ * Shizuka Kamishima - 2015-11-23
+ */
+
+var timerbar = document.getElementById("timerbar");
+var timertitle = document.getElementById("timertitle");
+var timerclock = document.getElementById("timerclock");
+
+
