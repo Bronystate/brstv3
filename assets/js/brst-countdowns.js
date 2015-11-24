f478ed7c2e4b35868080806b42118ee221ce6849
Shizuka <shizuka.kamishima@gmail.com>
2015-11-23 18:15:29 -0600

    added url injection to timerbar

--- assets/js/brst-countdowns.js
+++ assets/js/brst-countdowns.js
@@ -6,12 +6,12 @@
 var timerbar = document.getElementById("timerbar");
 var timertitle = document.getElementById("timertitle");
 var timerclock = document.getElementById("timerclock");
-var sidebar = document.getElementById("sidebar");
+var sidetimers = document.getElementById("sidetimers");
 
 var interval; //to hold the setInterval()
 
-var nextFridayMovie = { title: "Friday Movie Night", moment: moment.tz("America/New_York").hour(19).minute(0).second(0).millisecond(0).day(5) };
-var nextSaturdayMovie = { title: "Saturday Movie Night", moment: moment.tz("America/New_York").hour(14).minute(0).second(0).millisecond(0).day(6) };
+var nextFridayMovie = { title: "Friday Movie Night", moment: moment.tz("America/New_York").hour(19).minute(0).second(0).millisecond(0).day(5), url: "" };
+var nextSaturdayMovie = { title: "Saturday Movie Night", moment: moment.tz("America/New_York").hour(14).minute(0).second(0).millisecond(0).day(6), url: "" };
 
 function loadTimers() {
   console.log("Adding moments to timers...");
@@ -47,7 +47,11 @@ function update() {
     shown++;
     if (!titledone) {
       var time = calcRemaining(timer.moment);
-      timertitle.innerHTML = timer.title;
+      var title = timer.title;
+      if (timer.url !== "undefined" && timer.url != "") {
+        title = '<a href="' + timer.url + '">' + title + '</a>';
+      }
+      timertitle.innerHTML = title + " in";
       timerclock.innerHTML = time;
       titledone = true;
     }
